70638d219edc3336ca4e8ec66de9925b5def4592
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-06-03 15:55:08 +0700

    initialize

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+<body>
+
+    <h1>Homepage</h1>
+
+    <p>ini adalah variable <span id="greeting">greeting</span> </p>
+    <p> {{ $greeting }}</p>
+    
+</body>
+</html>
